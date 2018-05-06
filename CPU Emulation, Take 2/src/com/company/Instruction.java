5e76aeb0dd75798748cb1deac2b237cb9840e748
Tristan <cox.tristan.tc@gmail.com>
2018-05-06 00:25:55 -0500

    Update Instruction.java

--- CPU Emulation, Take 2/src/com/company/Instruction.java
+++ CPU Emulation, Take 2/src/com/company/Instruction.java
@@ -7,15 +7,11 @@ public class Instruction {
 
     public Instruction(String operation){
         opcode = Byte.parseByte(operation.substring(0, 1), 16);
-        boolean helper = false;
-        helper = opcode == 1 || opcode == 7 || opcode == 8;
+        boolean helper = opcode == 1 || opcode == 7 || opcode == 8;
         operand1 = Byte.parseByte(operation.substring(1, (helper) ? 2:3), 16);
         operand2 = Byte.parseByte(operation.substring((helper) ? 3:2), 16);
-
-
     }
-
-
+    
     public String toString(){
         return String.format("op:%d p1:%d p2:%d", opcode, operand1, operand2);
     }
